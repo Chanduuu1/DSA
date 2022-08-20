04ee51e652dec2279c1394c15567e54fda9c2d31
Chanduuu1 <shekharkrishnan13@gmail.com>
2022-08-21 00:58:08 +0530

    Added another optimized approch for time complexity O(n).

--- Trapping_RainWater.java
+++ Trapping_RainWater.java
@@ -2,12 +2,11 @@ import java.util.*;
 public class Trapping_RainWater{
     public static void WaterArea(int h[]){
         int maxLeftAux[] = new int[h.length];
-        int maxL = Integer.MIN_VALUE;
         int maxRightAux[] = new int[h.length];
-        int maxR = Integer.MIN_VALUE;
 
         //For maxL
         for(int i = 0; i < h.length; i++){
+            int maxL = Integer.MIN_VALUE;
             for(int j = 0; j <= i ; j++){ // note:- the code will 1st compare max h with itself 1st, which is what we want
                 if(h[j] > maxL){
                     maxL = h[j];  
@@ -17,14 +16,29 @@ public class Trapping_RainWater{
         }
         //For maxR
         for(int i = 0; i < h.length ; i++){
+            int maxR = Integer.MIN_VALUE;
             for(int j = (h.length - 1) ; j >= i ; j--){
                 if(h[j] > maxR){
                     maxR = h[j];
                 }
             }
             maxRightAux[i] = maxR;
+            System.out.println(maxRightAux[i]);
         }
-        
+/*
+// TIME COMPLEXITY FOR THIS IS O(n2) faltu me you used nested for,  optimize it as follows.
+
+        //For maxL
+        maxLeftAux[0] = h[0];
+        for(int i = 1; i < h.length ; i++){
+            maxLeftAux[i] = Math.max(h[i], maxLeftAux[i-1]);
+        }
+        //For maxR
+        maxRightAux[h.length-1] = h[h.length-1];
+        for(int i = h.length - 2; i >= 0 ;i--){
+            maxRightAux[i] = Math.max(h[i],maxRightAux[i+1]);
+        }
+*/
         // now the formula area = SIGMA (waterLVL - height of bar) * width
         
         int areaSum = 0;
